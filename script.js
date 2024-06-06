9ebe5c65c0312d2e55a310fd503df7ed4c77d296
Loganathan093 <82698689+Loganathan093@users.noreply.github.com>
2024-06-06 12:07:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,35 +10,15 @@ var num = document.getElementById("n");
             clearInterval(countdown)
             num.textContent = "Please enter coupon code given to you"
             form.classList.remove("dis")
-        }
-    }, 1000);
-
-    document.getElementById("btn").addEventListener("click", function(){
-        var inp = document.getElementById("cc").value;
-        var price = document.getElementById("rs");
-        
+            var price = document.getElementById("rs");
             num.textContent = "Coupon Code Applied";
             document.getElementById("cc").value = "";
             form.classList.add("dis");
+
             setInterval(() => {
-                num.classList.add("dis");
-                price.classList.remove("dis");
-            }, 1000);
-       
-    })
-    // function pay(){
-    //     var inp = document.getElementById("cc").value;
-    //     var price = document.getElementById("rs");
-    //         if(inp == "keerthi"){
-    //         num.textContent = "Coupon Code Applied :)";
-    //         document.getElementById("cc").value = "";
-    //         form.classList.add("dis");
-    //         setInterval(() => {
-    //             num.classList.add("dis");
-    //             price.classList.remove("dis");
-    //         }, 1000);
-    //     }
-    //     else{
-    //         num.textContent = 'Please enter "keerthi" eruma maadu'
-    //     }
-    // }
+            num.classList.add("dis");
+            price.classList.remove("dis");
+             }, 1000);  
+
+        }
+    }, 1000);
